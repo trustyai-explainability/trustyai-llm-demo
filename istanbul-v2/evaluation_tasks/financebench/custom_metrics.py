eb8076ee0fb331ca00ccbc846e21077d4b96501c
Rob Geada <rob@geada.net>
2026-02-11 15:45:40 +0000

    Istanbul updates

--- istanbul-v2/evaluation_tasks/financebench/custom_metrics.py
+++ istanbul-v2/evaluation_tasks/financebench/custom_metrics.py
@@ -14,16 +14,18 @@ def argmax(array, idx=None):
     return max_idx
 
 def evaluate(doc, loglikelihoods):
-    result = {"false_positive": 0, "true_positive": 0, "true_negative": 0, "false_negative": 0}
+    result = {"false_positive": 0, "true_positive": 0, "true_negative": 0, "false_negative": 0, "acc": 0}
     model_answer = argmax(loglikelihoods, 0)
     if doc['answer'] == "good" and model_answer == 0:
         result['true_positive'] = 1
+        result['acc'] = 1
     elif doc['answer'] == "good" and model_answer == 1:
         result['false_positive'] = 1
     elif doc['answer'] == "bad" and model_answer == 0:
         result['false_negative'] = 1
     else:
-        result['true_negative'] = 0
+        result['true_negative'] = 1
+        result['acc'] = 1
     return result
 
 def count(x):
